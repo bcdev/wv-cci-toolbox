34c47ccb8f2b003465f8defa2a221251bc07708c
dolaf <olaf.danne@brockmann-consult.de>
2020-10-09 18:15:12 +0200

    improved script for quicklooks

--- snap-tcwv/src/test/java/org/esa/snap/wvcci/tcwv/l3/L3UncertaintiesTest.java
+++ snap-tcwv/src/test/java/org/esa/snap/wvcci/tcwv/l3/L3UncertaintiesTest.java
@@ -100,7 +100,7 @@ public class L3UncertaintiesTest {
         System.out.println("sigma_TRUE_sqr = " + sigma_TRUE_sqr);
 
         // eq. (5):
-        float sigma_FINAL_sqr = sigma_TRUE_sqr/counts + c*meanUnc*meanUnc + (1.0f - c) * sumSqrUnc / (counts*counts);
+        float sigma_FINAL_sqr = sigma_TRUE_sqr / counts + c * meanUnc * meanUnc + (1.0f - c) * sumSqrUnc / (counts * counts);
         System.out.println("sigma_FINAL_sqr = " + sigma_FINAL_sqr);
     }
 
@@ -183,7 +183,64 @@ public class L3UncertaintiesTest {
         System.out.println("sigma_TRUE_sqr = " + sigma_TRUE_sqr);
 
         // eq. (5):
-        float sigma_FINAL_sqr = sigma_TRUE_sqr/counts + c*meanUnc*meanUnc + (1.0f - c) * sumSqrUnc / (counts*counts);
+        float sigma_FINAL_sqr = sigma_TRUE_sqr / counts + c * meanUnc * meanUnc + (1.0f - c) * sumSqrUnc / (counts * counts);
         System.out.println("sigma_FINAL_sqr = " + sigma_FINAL_sqr);
+    }
+
+    @Test
+    public void testTcwvErrors() {
+        // special case: constant uncertainties
+        final double uncertainty_const = 1.2;
+
+        // mean uncertainty, eq. (2):
+        double uncertainties_mean = 0.0;
+        for (int i = 0; i < 100; i++) {
+            uncertainties_mean += uncertainty_const;
+        }
+        uncertainties_mean /= 100.0;
+        System.out.println("uncertainties_mean CONST= " + uncertainties_mean);
+
+        // root of mean squared uncertainties, eq. (3):
+        double uncertainties_squared_mean = 0.0;
+        for (int i = 0; i < 100; i++) {
+            uncertainties_squared_mean += 1.44;
+        }
+        uncertainties_squared_mean /= 100;
+        double uncertainties_squared_mean_root = Math.sqrt(uncertainties_squared_mean);
+        System.out.println("uncertainties_squared_mean_root CONST = " + uncertainties_squared_mean_root);
+
+
+
+        // desert, 'slightly inhomogeneous' TCWV 10x10 window
+        double[] uncertainties = new double[]{
+                1.281, 1.273, 1.18, 1.216, 1.233, 1.241, 1.242, 1.253, 1.266, 1.276,
+                1.25, 1.303, 1.19, 1.233, 1.245, 1.249, 1.255, 1.264, 1.277, 1.275,
+                1.244, 1.214, 1.217, 1.238, 1.245, 1.253, 1.27, 1.273, 1.285, 1.283,
+                1.295, 1.177, 1.23, 1.222, 1.243, 1.202, 1.247, 1.258, 1.283, 1.276,
+                1.269, 1.169, 1.23, 1.18, 1.176, 1.198, 1.248, 1.257, 1.272, 1.261,
+                1.246, 1.312, 1.168, 1.194, 1.185, 1.273, 1.235, 1.237, 1.258, 1.257,
+                1.22, 1.245, 1.262, 1.263, 1.283, 1.28, 1.236, 1.205, 1.233, 1.249,
+                1.27, 1.272, 1.275, 1.282, 1.268, 1.225, 1.206, 1.189, 1.248, 1.221,
+                1.275, 1.284, 1.287, 1.286, 1.247, 1.241, 1.194, 1.207, 1.198, 1.198,
+                1.285, 1.286, 1.285, 1.27, 1.262, 1.223, 1.171, 1.247, 1.205, 1.242
+        };
+
+        // mean uncertainty, eq. (2):
+        uncertainties_mean = 0.0;
+        for (int i = 0; i < uncertainties.length; i++) {
+            uncertainties_mean += uncertainties[i];
+        }
+        uncertainties_mean /= uncertainties.length;
+        System.out.println("uncertainties_mean = " + uncertainties_mean);
+
+        // root of mean squared uncertainties, eq. (3):
+        uncertainties_squared_mean = 0.0;
+        for (int i = 0; i < uncertainties.length; i++) {
+            uncertainties_squared_mean += (uncertainties[i] * uncertainties[i]);
+        }
+        uncertainties_squared_mean /= uncertainties.length;
+        uncertainties_squared_mean_root = Math.sqrt(uncertainties_squared_mean);
+        System.out.println("uncertainties_squared_mean_root = " + uncertainties_squared_mean_root);
+
     }
 }
--- src/main/python/nctcwv2png.py
+++ src/main/python/nctcwv2png.py
@@ -11,19 +11,20 @@ import numpy as np
 import os
 import sys
 import copy
-from datetime import datetime,date,time
-#from enthought.pyface.ui.wx.grid.grid import Grid
-#from matplotlib.colors import NP_CLIP_OUT
-#from scikits.statsmodels.sandbox.regression.kernridgeregress_class import plt_closeall
+from datetime import datetime, date, time
+# from enthought.pyface.ui.wx.grid.grid import Grid
+# from matplotlib.colors import NP_CLIP_OUT
+# from scikits.statsmodels.sandbox.regression.kernridgeregress_class import plt_closeall
 from matplotlib import cm
 
 dpi = 80.0
 margin = 0.00
 
-default_cmap='gist_stern'
-default_cmap='gnuplot2'
-default_cmap='jet'
-creationYear='2014'
+default_cmap = 'gist_stern'
+default_cmap = 'gnuplot2'
+default_cmap = 'jet'
+creationYear = '2020'
+
 
 # def readcdict(lutfile, minV, maxV):
 #     if (lutfile!=None):
@@ -51,7 +52,6 @@ creationYear='2014'
 #         return None
 
 
-
 # def toDateStr(dateIn):
 #
 # 	year=date[0:4]
@@ -73,58 +73,58 @@ creationYear='2014'
 #                 out+=" - "+dt.strftime('%B')
 #
 # 	return out
-    
-def plotImag(array, outPNG, band, date, tickets, cdict):
-    #matplotlib.rcParams.update({'font.size': 8})
 
-    #imgplot = plt.imshow(array)
-    #imgplot.set_cmap('Set1')
-    #array=array.T
+def plotImag(array, outPNG, band, date, tickets, cdict):
+    # matplotlib.rcParams.update({'font.size': 8})
+
+    # imgplot = plt.imshow(array)
+    # imgplot.set_cmap('Set1')
+    # array=array.T
     # fig = plt.figure(figsize=figsize, dpi=dpi)
     fig = plt.figure(figsize=figsize)
-    ax1 = fig.add_axes([margin, margin, 1 - 2*margin, 1 - 2*margin])
-    
-    my_cmap=default_cmap
+    ax1 = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
+
+    my_cmap = default_cmap
     # bla = cm.get_cmap(my_cmap, 12)
     # print(bla(range(100)))
-    if(cdict != None):
-    	my_cmap = mpl.colors.LinearSegmentedColormap('my_colormap',cdict,256)
-    	
-    im=ax1.imshow(array, cmap=my_cmap)
-    
+    if (cdict != None):
+        my_cmap = mpl.colors.LinearSegmentedColormap('my_colormap', cdict, 256)
+
+    im = ax1.imshow(array, cmap=my_cmap)
+
     plt.xticks([])
     plt.yticks([])
-    #plt.title(date)
-    res=360.0/float(len(array[0]))  
-    cbaxes = fig.add_axes([0.05, 0.1, 0.05, 0.6]) 
-    cbar=plt.colorbar(im, ticks=tickets, cax=cbaxes)
+    # plt.title(date)
+    res = 360.0 / float(len(array[0]))
+    cbaxes = fig.add_axes([0.05, 0.1, 0.05, 0.6])
+    cbar = plt.colorbar(im, ticks=tickets, cax=cbaxes)
     cbar.ax.yaxis.set_tick_params(color='black')
-    for label in cbar.ax.get_yticklabels():  
-            label.set_color(colorTxt)
-            label.set_size(int((4/res)))
-    
+    for label in cbar.ax.get_yticklabels():
+        label.set_color(colorTxt)
+        label.set_size(int((4 / res)))
 
-    #print 'RESOLUTION: ', res
-    ax1.text(0.67, 0.2, date,
-        verticalalignment='bottom', horizontalalignment='right',
-        transform=ax1.transAxes,
-        color=colorTxt, fontsize=int(7/res),fontstyle='oblique')
-    
-    ax1.text(0.12, 0.72, band,
-        verticalalignment='bottom', horizontalalignment='right',
-        transform=ax1.transAxes,
-        color=colorTxt, fontsize=int(5/res), fontstyle='oblique')
-  
-    ax1.text(0.2, 0.02, "ImagingGroup.MSSL.UCL("+creationYear+")",
-        verticalalignment='bottom', horizontalalignment='right',
-        transform=ax1.transAxes,
-        color='black', fontsize=int(2.5/res), fontstyle='oblique')
+    # print 'RESOLUTION: ', res
+    ax1.text(0.95, 0.05, 'TCWV - ' + date,
+             verticalalignment='bottom', horizontalalignment='right',
+             transform=ax1.transAxes,
+             color=colorTxt, fontsize=int(7 / res), fontstyle='oblique')
+
+    ax1.text(0.098, 0.71, band.upper(),
+             verticalalignment='bottom', horizontalalignment='right',
+             transform=ax1.transAxes,
+             color=colorTxt, fontsize=int(5 / res), fontstyle='oblique')
+
+    ax1.text(0.95, 0.01, "ESA/CM-SAF (" + creationYear + ")",
+             verticalalignment='bottom', horizontalalignment='right',
+             transform=ax1.transAxes,
+             color=colorTxt, fontsize=int(5 / res), fontstyle='oblique')
 
     # plt.show()
     plt.savefig(outPNG)
-                    
+
     # plt.clf()
     # plt.close()
+
 
 if __name__ == "__main__":
 
@@ -143,45 +143,47 @@ if __name__ == "__main__":
     # bandsDis = sys.argv[9]
     bandsDis = 'tcwv'
 
-    numTicks=4
+    numTicks = 4
 
     w, h = int(size.split("x")[0]), int(size.split("x")[1])
-    figsize =  (1 + margin) * w / dpi, (1 + margin) * h / dpi
+    figsize = (1 + margin) * w / dpi, (1 + margin) * h / dpi
 
-    max=[]
-    min=[]
+    max = []
+    min = []
 
-    i=0
+    i = 0
     for val in min_max:
         # min.insert(i, float(val.split(':')[0]));
         min.insert(i, 0.0)
         # max.insert(i, float(val.split(':')[1]));
         max.insert(i, 70.0)
-        #print bands[i], 'min, max', min[i],max[i]
-        i+=1
+        # print bands[i], 'min, max', min[i],max[i]
+        i += 1
     # name=inFile.split('/')[-1].replace('.nc','')
-    name=os.path.splitext(os.path.basename(inFile))[0]
+    name = os.path.splitext(os.path.basename(inFile))[0]
     print('inFile: ', inFile)
     print('name: ', name)
-    ncfile = nc.Dataset(inFile,'r')
-    #data= ncfile.variables[band][0,:]
+    ncfile = nc.Dataset(inFile, 'r')
+    # data= ncfile.variables[band][0,:]
     # NOTE: Alex Loew requested a 3rd dimension (time). For old mosaic netcdf (2D) use this:
     # data= ncfile.variables[band][:]
     # w=np.where(np.isnan(data))
     # data[w]=0.0
     # data=np.where(data<=max[i], data, max[i])
     # data=np.where(data>min[i], data, 0.0)
-    #print np.min(data), np.max(data)
+    # print np.min(data), np.max(data)
     # data[0,0]=min[i]
     # data[-1,-1]=max[i]
-    bin=(min[0]+max[0])/float(numTicks)
-    tickets=[min[0]+(j*bin) for j in range(numTicks+1)]
+    bin = (min[0] + max[0]) / float(numTicks)
+    tickets = [min[0] + (j * bin) for j in range(numTicks + 1)]
     # cdict=readcdict(lutColor, min[i], max[i])
-    cdict=None
-    outdir=outDir
+    cdict = None
+    outdir = outDir
     # os.system('mkdir -p '+outdir)
     # dateStr=toDateStr(date0)
-    dateStr='20200101'  # todo
+    date_start_index = name.find('20')
+    date_end_index = name.find('fv') - 1
+    date_str = name[date_start_index:date_end_index]
 
     tcwv_arr = np.array(ncfile.variables['tcwv'])
     # tcwv_arr[np.where(np.isnan(tcwv_arr))] = min[0]
@@ -189,9 +191,8 @@ if __name__ == "__main__":
     tcwv_arr[np.where(np.isnan(tcwv_arr))] = -15.0
 
     # plotImag(data,outdir+'/'+name+"_tcwv.png", 'tcwv', dateStr, tickets, cdict)
-    plotImag(tcwv_arr,outdir+'/'+name+"_tcwv.png", 'tcwv', dateStr, tickets, cdict)
-    i+=1
+    plotImag(tcwv_arr, outdir + '/' + name + "_tcwv.png", 'tcwv', date_str, tickets, cdict)
+    i += 1
 
     ncfile.close()
     exit(0)
-
